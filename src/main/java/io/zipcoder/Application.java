39df4a816ae2404479c5c162f818a331b96d8a56
Erick Tepale <e.tepale@gmail.com>
2019-06-24 15:55:06 -0400

    finished part 3

--- src/main/java/io/zipcoder/Application.java
+++ src/main/java/io/zipcoder/Application.java
@@ -3,6 +3,7 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Application {
     public static void main(String[] args) {
@@ -30,10 +31,27 @@ public class Application {
             }
         }
 
-
-        System.out.println(listPets);
+        // basic sort using comporable
         Collections.sort(listPets);
 
+        //using annonymous comparator to sort by type then by name
+        Collections.sort(listPets, new Comparator<Pet>() {
+            @Override
+            public int compare(Pet o1, Pet o2) {
+                if(o1.getType().compareTo(o2.getType()) > 0)
+                    return 1;
+                else if(o1.getType().compareTo(o2.getType()) < 0)
+                    return -1;
+                else{
+                    if(o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()) > 0)
+                        return 1;
+                    else if(o1.getName().toLowerCase().compareTo(o2.getName().toLowerCase()) < 0)
+                        return -1;
+                    else
+                        return 0;
+                }
+            }
+        });
         System.out.println(listPets);
 
 
